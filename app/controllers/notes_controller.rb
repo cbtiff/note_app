717eefa10506f4dfa46b5b7ddac3fa361db0bfd1
Tiffany Bigelow <cbtiff@gmail.com>
2018-02-20 20:58:31 -0700

    update 1

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,17 @@
 class NotesController < ApplicationController
   def index
+    @notes = Note.all
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def edit
+    @note = Note.edit
   end
 
   def new
+    @note = Note.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'notes/index'
+  root 'notes#index'
 
-  get 'notes/show'
-
-  get 'notes/edit'
-
-  get 'notes/new'
+resources :notes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
