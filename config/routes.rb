f75b0335deeba76dff221a0426120cfc02e1a30e
Brock Beuch <brockbeuch@gmail.com>
2018-02-20 19:16:32 -0700

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'notes/index'
+
+  get 'notes/show'
+
+  get 'notes/edit'
+
+  get 'notes/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
